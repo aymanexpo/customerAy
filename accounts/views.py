e2fa545519d666ef36f79e0cb60cf7121aedce6e
aymane talibi <aymane.talibi.at@gmail.com>
2020-09-24 23:27:59 +0100

    in progress

--- accounts/views.py
+++ accounts/views.py
@@ -33,9 +33,9 @@ def customer(request, pk_test):
     return render(request, 'accounts/customers.html', context)
 
 def createOrder(request, pk):
-    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'))
+    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'), extra=10)
     customer=Customer.objects.get(id=pk)
-    formset=OrderFormSet(instance=customer)
+    formset=OrderFormSet(queryset=Order.objects.none(), instance=customer)
     #form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
         #print('Printing POST:', request.POST)
